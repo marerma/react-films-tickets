690ec35cba2aef8b6d7415732735714929f9c321
marerma <maria.kadantseva@gmail.com>
2023-06-20 18:47:36 +0300

    feat: add api requests

---
+++ client/api/api.d.ts
@@ -0,0 +1,24 @@
+interface Movie {
+  title: string;
+  posterUrl: string;
+  releaseYear: number,
+  description:string;
+  genre: string;
+  id: string;
+  rating: number,
+  director: string;
+  reviewIds: string[];
+}
+
+interface Cinema {
+  id: string;
+  name: string;
+  movieIds: Pick<Movie, id>[];
+}
+
+interface Review {
+  id: string;
+  name: string;
+  text: string;
+  rating: number,
+}
---
+++ client/api/index.ts
@@ -0,0 +1,11 @@
+import {getAllFilms, getCinemaFilms} from './getFilms';
+import {getOneFilm} from './getOneFilm';
+import {getReviews, getFilmReviews} from './getReviews';
+
+export const api = {
+  getAllFilms, 
+  getCinemaFilms,
+  getOneFilm,
+  getReviews,
+  getFilmReviews
+}
