6d715f003a906ce5924e8058ed613d4823b43ff5
marerma <maria.kadantseva@gmail.com>
2023-06-23 01:49:11 +0300

    feat: add localization for genres and filter films

---
+++ client/shared/utils/Localization.ts
@@ -0,0 +1,24 @@
+const GENRES: {[key: string]: {ru: string, en: string}} = {
+  horror: {
+    ru: 'Ужасы',
+    en: 'horror'
+  },
+  action: {
+    ru: 'Боевик',
+    en: 'action'
+  },
+  comedy: {
+    ru: 'Комедия',
+    en: ''
+  },
+  fantasy: {
+    ru: 'Фэнтези',
+    en: 'fantasy'
+  },
+}
+
+
+const translateGenre = (genre: string) => {
+  return GENRES[genre].ru;
+}
+export { translateGenre };
