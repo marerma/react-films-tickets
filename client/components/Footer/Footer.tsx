e286a7e09b730aa4c8d28db4f6405c807be9351a
marerma <maria.kadantseva@gmail.com>
2023-06-23 01:55:34 +0300

    refactor: adjust components imports

--- client/components/Footer/Footer.tsx
+++ client/components/Footer/Footer.tsx
@@ -1,14 +1,16 @@
-import Link from 'next/link';
-import { ROUTES } from '../../data/Routes';
-import styles from './Footer.module.sass';
+import Link from 'next/link'
+import { ROUTES } from 'shared/constants'
+import styles from './Footer.module.sass'
 
-export const Footer = () => {
+const Footer = () => {
   return (
-  <footer className={styles.footer}>
-    <div className={styles.footer__wrapper}>
-      <Link href={ROUTES.faq}>Вопросы-ответы</Link>
-      <Link href={ROUTES.about}>О нас</Link>
-    </div>
-  </footer>
+    <footer className={styles.footer}>
+      <div className={styles.footer__wrapper}>
+        <Link href={ROUTES.faq}>Вопросы-ответы</Link>
+        <Link href={ROUTES.about}>О нас</Link>
+      </div>
+    </footer>
   )
 }
+
+export default Footer
--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,15 +1,21 @@
-import Link from 'next/link';
-import { ROUTES } from '../../data/Routes';
-import { Cart } from '../Cart/Cart';
-import styles from './Header.module.sass';
+import Link from 'next/link'
+import { ROUTES } from 'shared/constants'
+import { Cart } from 'components'
+import styles from './Header.module.sass'
 
-export const Header = () => {
+const Header = () => {
   return (
-  <header className={styles.header}>
-    <div className={styles.header__wrapper}>
-      <h1><Link href={ROUTES.home}>Билетопоиск</Link></h1>
-      <Cart/>
-    </div>
-  </header>
+    <header className={styles.header}>
+      <div className={styles.header__wrapper}>
+        <h1>
+          <Link href={ROUTES.home}>Билетопоиск</Link>
+        </h1>
+        <Link href={ROUTES.cart}>
+          <Cart />
+        </Link>
+      </div>
+    </header>
   )
 }
+
+export default Header
