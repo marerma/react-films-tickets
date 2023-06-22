33896c48bd81e30aba3a62b93b46bb6e2d4ed02f
marerma <maria.kadantseva@gmail.com>
2023-06-23 01:52:39 +0300

    feat: add loader

---
+++ client/components/Loader/Loader.tsx
@@ -0,0 +1,18 @@
+import styles from './Loader.module.sass'
+
+const Loader = () => {
+  return (
+    <div className={styles.lds_spinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader
