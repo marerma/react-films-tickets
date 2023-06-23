dc2d1cfc6ff6430f5d1919bc9a7a8d038e09df01
marerma <maria.kadantseva@gmail.com>
2023-06-23 14:41:04 +0300

    refactor: extract components from cart page

---
+++ client/components/Cart/CartContent.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useAppSelector } from 'store/hooks'
+import { FilmsInCartSelector, TotalAmount } from 'store/selector/Selectors'
+import { useMemo } from 'react'
+import { useGetAllFilmsQuery } from 'store/reducer/FilmsApiSlice'
+import { Films } from 'components'
+import styles from './Cart.module.sass'
+import { CART_RANGE } from 'shared/constants'
+
+const CartContent = () => {
+  const filmsInCart = useAppSelector(FilmsInCartSelector);
+  const totalAmount = useAppSelector(TotalAmount);
+  const { data } = useGetAllFilmsQuery();
+
+  const selectedFilms = useMemo(() => {
+    if (filmsInCart && data) {
+      const ids = filmsInCart.map((el) => el.id)
+      return data.filter((film) => ids.includes(film.id))
+    }
+  }, [data, filmsInCart])
+
+  return (
+    <div className={styles.cart__wrapper}>
+      {selectedFilms && <Films.List allFilms={selectedFilms} isRemovable />}
+      <div className={totalAmount === CART_RANGE.min? styles.cart__total_empty : styles.cart__total}>
+        {' '}
+        {totalAmount === CART_RANGE.min
+          ? <span className={styles.cart__text}>В корзине нет билетов</span>
+          : <div className={styles.cart__textWrapper}>
+              <span className={styles.cart__text}>Итого билетов:</span>
+              <span className={styles.cart__text}>{totalAmount}</span>
+            </div>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default CartContent;
--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,32 +1,11 @@
-'use client'
-
-import { useAppSelector } from 'store/hooks'
-import { FilmsInCartSelector, TotalAmount } from 'store/selector/Selectors'
-import { useMemo } from 'react'
-import { useGetAllFilmsQuery } from 'store/reducer/FilmsApiSlice'
-import { Films } from 'components'
-import styles from './Cart.module.sass'
-
+import { Metadata } from 'next'
+import { CartContent } from "components"
+ 
+export const metadata: Metadata = {
+  title: 'Tickets',
+  description: 'Tickets Cinema',
+}
+ 
 export default function Cart() {
-  const filmsInCart = useAppSelector(FilmsInCartSelector)
-  const totalAmount = useAppSelector(TotalAmount)
-  const { data } = useGetAllFilmsQuery()
-  const selectedFilms = useMemo(() => {
-    if (filmsInCart && data) {
-      const ids = filmsInCart.map((el) => el.id)
-      return data.filter((film) => ids.includes(film.id))
-    }
-  }, [data, filmsInCart])
-
-  return (
-    <div className={styles.cart__wrapper}>
-      {selectedFilms && <Films allFilms={selectedFilms} isRemovable />}
-      <div className={styles.cart__total}>
-        {' '}
-        {totalAmount === 0
-          ? 'В корзине нет билетов'
-          : `Всего билетов: ${totalAmount}`}
-      </div>
-    </div>
-  )
+  return <CartContent />
 }
