e272395abf3af2da4afa03f44066175c33e4b9d3
marerma <maria.kadantseva@gmail.com>
2023-06-23 14:44:51 +0300

    feat: add debounce to search field

--- client/components/Filters/SearchFilm.tsx
+++ client/components/Filters/SearchFilm.tsx
@@ -1,22 +1,37 @@
 'use client'
-
-import { useState } from 'react'
-import styles from './Filters.module.sass'
+import debounce from 'lodash.debounce'
+import { useState, useMemo, useEffect } from 'react'
 import { useAppDispatch } from 'store/hooks'
-import { addFilter } from 'store/reducer/FiltersSlice'
+import { addFilter, resetFilters } from 'store/reducer/FiltersSlice'
+import styles from './Filters.module.sass'
 
 type SearchProps = {
   filterType: 'genre' | 'title' | 'cinema'
 }
-export const Search = ({ filterType }: SearchProps) => {
+const Search = ({ filterType }: SearchProps) => {
   const [searchValue, setSearchValue] = useState('')
   const dispatch = useAppDispatch()
 
+  const handleSearchDebounced = useMemo(
+    () =>
+      debounce((event: React.ChangeEvent<HTMLInputElement>, searchValue: string) => {
+        dispatch(addFilter({ type: filterType, value: searchValue }))
+      }),
+    [dispatch, filterType]
+  );
+  
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target
-    setSearchValue(value)
-    dispatch(addFilter({ type: filterType, value }))
+    const { value } = e.target;
+    setSearchValue(value);
+    handleSearchDebounced(e, value);
   }
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetFilters())
+    }
+  }, [dispatch])
+  
   return (
     <div className={styles.input}>
       <label htmlFor="titleSearch" className={styles.input__searchLabel}>
@@ -33,3 +48,5 @@ export const Search = ({ filterType }: SearchProps) => {
     </div>
   )
 }
+
+export default Search;
