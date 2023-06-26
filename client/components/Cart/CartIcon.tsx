974a3133da3c11b115c1b065d60f39e7eec0d628
marerma <maria.kadantseva@gmail.com>
2023-06-26 22:45:14 +0300

    fix: style of total amount iconand text  in cart

--- client/components/Cart/CartIcon.tsx
+++ client/components/Cart/CartIcon.tsx
@@ -1,13 +1,14 @@
 import { useAppSelector } from 'store/hooks'
 import styles from './Cart.module.sass'
 import { selectTotalCartAmount } from 'store/selector/Selectors'
+import { CART_RANGE } from 'shared/app-congif'
 
 const Cart = () => {
   const totalAmount = useAppSelector(selectTotalCartAmount)
 
   return (
     <div className={styles.cart}>
-      <div className={styles.cart__amount}>{totalAmount}</div>
+      {totalAmount > CART_RANGE.min && <div className={styles.cart__amount}>{totalAmount}</div>}
       <div className={styles.cart__icon} />
     </div>
   )
