0b2d40e3e5229fc7570301a134245e9a4fdc1282
marerma <maria.kadantseva@gmail.com>
2023-06-26 13:16:50 +0300

    feat: reorganize and rename selectors

--- client/store/selector/Selectors.ts
+++ client/store/selector/Selectors.ts
@@ -1,12 +1,24 @@
+import { createSelector } from '@reduxjs/toolkit';
 import { RootState } from 'store/store'
 
-const FilmsInCartSelector = (state: RootState) => state.cart.filmsInCart
-const TotalAmount = (state: RootState) =>
+const selectFilmsInCart = (state: RootState) => state.cart.filmsInCart;
+
+const selectFilmId = (state: RootState, id: string) => id;
+
+const selectFilmInCartById = createSelector([selectFilmsInCart, selectFilmId], (filmsInCart, id) => {
+  return filmsInCart.find((film) => film.id === id)?.amount || 0
+})
+
+const selectActiveFilters = (state: RootState) => state.filter;
+
+const selectTotalCartAmount = (state: RootState) =>
   state.cart.filmsInCart.reduce((acc, el) => {
     return acc + el.amount
-  }, 0)
-const IsActiveFilter = (state: RootState) =>
-  !Object.values(state.filter).every((item) => item.length === 0)
-const ActiveFilters = (state: RootState) => state.filter
+  }, 0);
 
-export { FilmsInCartSelector, TotalAmount, IsActiveFilter, ActiveFilters }
+const IsActiveFilter = (state: RootState) =>
+  !Object.values(state.filter).every((item) => item.length === 0);
+
+
+
+export { selectFilmsInCart, selectTotalCartAmount, IsActiveFilter, selectActiveFilters, selectFilmInCartById }
