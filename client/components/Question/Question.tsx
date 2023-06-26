15492ca326c5ccb21a05420257c14bd83f00a778
marerma <maria.kadantseva@gmail.com>
2023-06-26 13:38:57 +0300

    feat: add click on question title and change icon style

--- client/components/Question/Question.tsx
+++ client/components/Question/Question.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import styles from './Question.module.sass'
 
 const Question = ({ question, description }: QuestionData) => {
-  const [isOpen, setIsOpen] = useState(true)
+  const [isOpen, setIsOpen] = useState(false)
 
   const openTextHandler = () => {
     setIsOpen(!isOpen)
@@ -11,12 +11,12 @@ const Question = ({ question, description }: QuestionData) => {
 
   return (
     <div className={styles.question}>
-      <div className={styles.question__wrapper}>
+      <div className={styles.question__wrapper} onClick={openTextHandler}>
         <p className={styles.question__title}>{question}</p>
         {isOpen && <p className={styles.question__answer}>{description}</p>}
       </div>
       <div
-        className={isOpen ? styles.question__icon : styles.question__icon_open}
+        className={isOpen ? styles.question__icon_open : styles.question__icon}
         onClick={openTextHandler}
       />
     </div>
