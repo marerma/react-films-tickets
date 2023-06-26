dc825a469396de5c228eae8871772a30bd3d6ead
marerma <maria.kadantseva@gmail.com>
2023-06-26 13:25:01 +0300

    feat: optimize ticketscounter with new selectors and add replace magic strings with config data

--- client/components/TicketsCounter/TicketsCounter.tsx
+++ client/components/TicketsCounter/TicketsCounter.tsx
@@ -1,12 +1,12 @@
 'use client'
+
 import { useAppDispatch, useAppSelector } from 'store/hooks'
+import { selectFilmInCartById } from 'store/selector/Selectors'
+import { CART_RANGE } from 'shared/app-congif'
 import styles from './TicketsCounter.module.sass'
-import { FilmsInCartSelector } from 'store/selector/Selectors'
-import { useEffect, useMemo } from 'react'
 import {
   addToCart,
   decrementCart,
-  removeFromCart,
 } from 'store/reducer/CartSlice'
 
 const COLOR = {
@@ -14,23 +14,22 @@ const COLOR = {
   inactive: '#FFA375',
 }
 type TicketsCounterProps = {
-  id: string
-}
-const TicketsCounter = ({ id }: TicketsCounterProps) => {
-  const filmsInCart = useAppSelector(FilmsInCartSelector)
-  const dispatch = useAppDispatch()
+  id: string;
+  handleRemoveModal?: () => void;
+};
 
-  const totalAmount = useMemo(() => {
-    const film = filmsInCart.find((el) => el.id === id)
-    return film ? film.amount : 0
-  }, [filmsInCart, id])
+const TicketsCounter = ({ id, handleRemoveModal }: TicketsCounterProps) => {
+  const totalAmount = useAppSelector(state => selectFilmInCartById(state, id))  
+  const dispatch = useAppDispatch()
 
   const handleAdd = () => {
     dispatch(addToCart({ id }))
   }
 
   const handleRemove = () => {
-    dispatch(decrementCart({ id }))
+    if (handleRemoveModal && totalAmount === CART_RANGE.min + 1) {
+      handleRemoveModal()
+    } else dispatch(decrementCart({ id }))
   }
 
   return (
@@ -47,7 +46,7 @@ const TicketsCounter = ({ id }: TicketsCounterProps) => {
             width="20"
             height="20"
             rx="4"
-            fill={totalAmount === 0 ? COLOR.inactive : COLOR.active}
+            fill={totalAmount === CART_RANGE.min ? COLOR.inactive : COLOR.active}
           />
           <path
             d="M14.5 10C14.5 10.0995 14.4605 10.1948 14.3902 10.2652C14.3198 10.3355 14.2245 10.375 14.125 10.375H5.875C5.77554 10.375 5.68016 10.3355 5.60984 10.2652C5.53951 10.1948 5.5 10.0995 5.5 10C5.5 9.90054 5.53951 9.80516 5.60984 9.73484C5.68016 9.66451 5.77554 9.625 5.875 9.625H14.125C14.2245 9.625 14.3198 9.66451 14.3902 9.73484C14.4605 9.80516 14.5 9.90054 14.5 10Z"
@@ -68,7 +67,7 @@ const TicketsCounter = ({ id }: TicketsCounterProps) => {
             width="20"
             height="20"
             rx="4"
-            fill={totalAmount === 30 ? COLOR.inactive : COLOR.active}
+            fill={totalAmount === CART_RANGE.max? COLOR.inactive : COLOR.active}
           />
           <path
             d="M14.5 10C14.5 10.0995 14.4605 10.1948 14.3902 10.2652C14.3198 10.3355 14.2245 10.375 14.125 10.375H10.375V14.125C10.375 14.2245 10.3355 14.3198 10.2652 14.3902C10.1948 14.4605 10.0995 14.5 10 14.5C9.90054 14.5 9.80516 14.4605 9.73484 14.3902C9.66451 14.3198 9.625 14.2245 9.625 14.125V10.375H5.875C5.77554 10.375 5.68016 10.3355 5.60984 10.2652C5.53951 10.1948 5.5 10.0995 5.5 10C5.5 9.90054 5.53951 9.80516 5.60984 9.73484C5.68016 9.66451 5.77554 9.625 5.875 9.625H9.625V5.875C9.625 5.77554 9.66451 5.68016 9.73484 5.60984C9.80516 5.53951 9.90054 5.5 10 5.5C10.0995 5.5 10.1948 5.53951 10.2652 5.60984C10.3355 5.68016 10.375 5.77554 10.375 5.875V9.625H14.125C14.2245 9.625 14.3198 9.66451 14.3902 9.73484C14.4605 9.80516 14.5 9.90054 14.5 10Z"
@@ -80,4 +79,4 @@ const TicketsCounter = ({ id }: TicketsCounterProps) => {
   )
 }
 
-export default TicketsCounter
+export default TicketsCounter;
